b51767cdc24fcd18923ed68f03aa76b85e9cc86c
Anastasiya Pozyomina <44189472+AnastasiyaPozyomina@users.noreply.github.com>
2020-01-06 00:23:17 +0700

    add @media, plagine jquery:magnific popup

---
+++ js.js
@@ -0,0 +1,15 @@
+$('.portfolio').magnificPopup({
+    type: 'image',
+    delegate: 'a',
+    gallery: {
+        enabled: true
+    },
+    zoom: {
+        enabled: true,
+        duration:900
+   
+        
+    }
+    
+
+});
